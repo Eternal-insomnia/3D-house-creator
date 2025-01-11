f43adcf30bea10ced20ca6d5dcd0c14276f561ea
Лёхич <alexsuvorov2506@gmail.com>
2025-01-11 20:17:36 +0300

    Edited gitignore

--- backend/target/classes/database/initDB.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users"
-(
-    user_id UUID NOT NULL UNIQUE PRIMARY KEY,
-    user_name text NOT NULL,
-    user_surname text NOT NULL,
-    user_email text NOT NULL UNIQUE,
-    user_password text NOT NULL
-);
--- backend/target/classes/database/populateDB.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users VALUES 
-('80a98094-ccf7-49ab-a090-9394be7f9d1b', 'test_name', 'test_surname', 'user@test', 'user');
